875ad8396d6d68eaac86734ab6e381665aac9a3e
Han Hossain <hanhossain@outlook.com>
2024-08-20 00:52:12 -0700

    simple reqwest

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+
+#[tokio::main]
+async fn main() {
+    let a = reqwest::get("https://httpbin.org/ip")
+        .await
+        .unwrap()
+        .json::<HashMap<String, String>>()
+        .await
+        .unwrap();
+    println!("{a:#?}");
 }
