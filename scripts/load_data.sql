d1678ccca3cb7f6f4bd1f48cedb2d5c6eb3b4d89
Han Hossain <hanhossain@outlook.com>
2025-07-24 02:16:08 -0500

    load schedules

--- scripts/load_data.sql
+++ scripts/load_data.sql
@@ -1,3 +1,7 @@
 CREATE TABLE players AS
 SELECT unnest(map_values(json), recursive := true)
-FROM read_json('data/raw/players.json');
+FROM 'data/raw/players.json';
+
+CREATE TABLE schedules AS
+SELECT *, parse_path(filename)[4] AS season_type, parse_filename(filename, true) AS season
+FROM 'data/raw/schedules/*/*.json';
